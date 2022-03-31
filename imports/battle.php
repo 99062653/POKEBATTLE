3cfc9104ef1ebee8febeb6deb1206c713800498a
- <96921300+schwaaaa@users.noreply.github.com>
2022-03-31 13:01:42 +0200

    0.80: battle etca

---
+++ imports/battle.php
@@ -0,0 +1,15 @@
+<?php
+require "code/services/battleservice.php";
+?>
+
+<div id="Battle">
+    <div id="FriendlyPokemon">
+        
+    </div>
+    <div id="EnemyPokemon">
+        
+    </div>
+    <div id="Attacks">
+
+    </div>
+</div>
