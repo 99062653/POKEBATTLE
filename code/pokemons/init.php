15b94e1ce51163c109ff7e03327c4ff94a222c22
- <99062653@mydavinci.nl>
2022-04-05 11:58:34 +0200

    0.96: init.php

---
+++ code/pokemons/init.php
@@ -0,0 +1,4 @@
+<?php
+require "code/pokemons/pokemons.php";
+require "code/pokemons/Pokemon.php";
+require "code/pokemons/Pikachu.php";
--- code/services/indexservice.php
+++ code/services/indexservice.php
@@ -1,7 +1,5 @@
 <?php
-require "code/pokemons/pokemons.php";
-require "code/pokemons/Pokemon.php";
-require "code/pokemons/Pikachu.php";
+require "code/pokemons/init.php";
 
 function loadRandomBG() {
     $backgrounds = array(
@@ -21,16 +19,11 @@ function loadRandomBG() {
 
 //default pokemons 
 //voor Pikachu express een nieuwe class gemaakt om mijn skills te showen
+$AllPokemons = new Pokemons;
 $Pikachu = new Pikachu("Pikachu", "Electric", 100, array("Thunderbolt" => 30, "Quick Attack" => 25, "Thunder Shock" => 35), array("Fire", "Water", "Electric"), array("Fighting", "Flying", "Ground"), "img/pokemon/pikachu.png");
 $Charmender = new Pokemon("Charmander", "Fire", 100, array("Ember" => 40, "Fire Spin" => 20), array("Water", "Grass", "Fire"), array("Fighting", "Flying", "Ground"), "img/pokemon/charmander.png");
 $Bulbasaur = new Pokemon("Bulbasaur", "Grass", 100, array("Vine Whip" => 30, "Razor Leaf" => 25), array("Fire", "Water", "Electric"), array("Fighting", "Flying", "Ground"), "img/pokemon/bulbasaur.png");
 $Snorlax = new Pokemon("Snorlax", "Normal", 120, array("Headbutt" => 35, "Hyper Beam" => 50), array("Fire", "Water", "Electric"), array("Fighting", "Flying", "Ground"), "img/pokemon/snorlax.png");
 
-$AllPokemons = New Pokemons;
 $FriendlyPokemon = $AllPokemons->getPokemonByName($_GET["chosenpokemon"]);
 $EnemyPokemon = $AllPokemons->getPokemonByName($_GET["enemypokemon"]);
-$yourTurn = true;
-
-if (isset($_POST["Action"])) {
-    $FriendlyPokemon->attackPokemon($EnemyPokemon, $_POST["Attack"]);
-}
