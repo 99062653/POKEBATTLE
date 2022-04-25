a880fe28901511af4d9873d4d3a9e1687e27953b
- <99062653@mydavinci.nl>
2022-04-25 09:11:56 +0200

    comments

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-function addInput($id) {
+function addInput($id) { // meerdere inputs toevoeging (voor attacks bijvoorbeeld)
     var element = document.getElementById($id);
     var input = document.createElement("input");
     var inputModifier = document.createElement("input");
