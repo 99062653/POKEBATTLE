9aab6d91b68462a9b4689fee9c4cc8449f0ebf31
- <99062653@mydavinci.nl>
2022-04-26 09:15:56 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -4,3 +4,21 @@ function addInput($id) { // meerdere inputs toevoeging (voor attacks bijvoorbeel
     var inputModifier = document.createElement("input");
 
 }
+
+var msg = document.title;
+var speed = 150;
+var endChar = "... ";
+var pos = 0;
+
+function moveTitle() {
+    var ml = msg.length;
+
+    title = msg.substring(pos, ml) + endChar + msg.substring(0, pos);
+    document.title = title;
+
+    pos++;
+    if (pos > ml) pos = 0;
+    window.setTimeout("moveTitle()", speed);
+}
+
+moveTitle(); 
